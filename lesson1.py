07d879f1b5d3cc8131042fab170d507d638f7488
Sergejvzh <Zhilenkov.72@mail.ru>
2024-11-24 12:19:59 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+print ("1 st program")
+print (9 ** 0.5 * 5) # 15
+print ("2nd program")
+print (9.99 > 9.98, 1000 != 1000.1) # True
+print ("3rd program")
+print (23 + 67) # 90
+print ("4th program")
+print (13 == 13 and 42 == 42, 13 == 13 or 42 != 42) # boolean
+
+
+
+
