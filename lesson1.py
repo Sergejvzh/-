238807d15c59aba16b9c2455219f8b6e968827d5
Sergejvzh <Zhilenkov.72@mail.ru>
2024-11-25 07:40:05 +0300

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,11 +1,12 @@
 print ("1 st program")
-print (9 ** 0.5 * 5) # 15
+print (9 ** 0.5 * 5)
 print ("2nd program")
-print (9.99 > 9.98, 1000 != 1000.1) # True
+print (9.99 > 9.98 and 1000 != 1000.1)
 print ("3rd program")
-print (23 + 67) # 90
+print (2 * 2 + 2),(2 * (2 +2))
+print (2 * 2 + 2 == 2 * (2 + 2))
 print ("4th program")
-print (13 == 13 and 42 == 42, 13 == 13 or 42 != 42) # boolean
+print (int(float('123.456') * 10) % 10)
 
 
 
