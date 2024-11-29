40b0241aab4c7327a5e7ef20310a45f89c6743e2
Sergejvzh <Zhilenkov.72@mail.ru>
2024-11-29 06:54:07 +0300

    Add files via upload

--- lesson5.py
+++ lesson5.py
@@ -1,11 +1,12 @@
 my_string = input(" Меня зовут: ")
 print(type("my_string"))
-# print (len(my_string)
+print (len(my_string))
 # print("my_string".upper())
 # print("my_string".lower())
-#print('привет, я Сергей'.replace(' ' , ''))
-print(my_string[0])
-print(my_string[-1])
+# print('привет, я Сергей'.replace(' ' , ''))
+# print(my_string[0])
+# print(my_string[-1])
+
 
 
 
