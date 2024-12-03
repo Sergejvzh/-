1b6aa6b989e0455d8e889f4d9b126254b1be4471
Sergejvzh <Zhilenkov.72@mail.ru>
2024-12-03 06:56:00 +0300

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,14 @@
+immutable_var = 1
+immutable_var2 = ('bobr')
+immutable_var3 = ([1,2,'a','b'])
+print(immutable_var)
+print(immutable_var2)
+print(immutable_var3)
+print(immutable_var3 [1]) # если взять индекс [1] кортежа, выдаст элемент "2"
+immutable_var3[1] = 5 # если заменить элемент на число "5" выдаст ошибку
+print(immutable_var)
+
+# mutable_list = [1, 2, 'a', 'b', 'Sergej']
+# print(mutable_list)
+# mutable_list [4] = 9 # если заменить [Sergej] кортежа на число 9, ошибки нет
+# print(mutable_list)
