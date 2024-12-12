5bd0456af1947eb8be684f2dbd6ab8be04fd5f9d
Sergejvzh <Zhilenkov.72@mail.ru>
2024-12-12 14:07:54 +0300

    Add files via upload

---
+++ lesson7.py
@@ -0,0 +1,20 @@
+#my_dict = {'Sergej': 1972, 'Artem': 1985,'Masha': 2001}
+#print(my_dict)
+#print(my_dict['Sergej'])
+#my_dict['Max'] = 1962
+#print(my_dict)
+#del my_dict['Masha']
+#print(my_dict)
+#my_dict.update({'Sahsa': 1997,'Denis': 2003})
+#print(my_dict)
+#a = my_dict.pop('Sergej')
+#print(a)
+#print(my_dict)
+
+my_set = {11,12,13,14,15,11,12,13,14,'String',True,(3, 4, 5)}
+print(my_set)
+print(my_set.discard(12))
+print(my_set)
+print(my_set.add(7))
+print(my_set.add('Denis'))
+print(my_set)
